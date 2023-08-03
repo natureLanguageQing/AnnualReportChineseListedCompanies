b36b579240855061a1edd7f3b2ba23d115f574c1
Yan GuangQing <624576166@qq.com>
2023-08-03 17:14:18 +0800

    dicengpinjieluojigengxin

--- form_inner.py
+++ form_inner.py
@@ -19,49 +19,52 @@ print(len(key_file))
 
 # wenjianmingcheng duiyingwenjianmingchengzhongchuxianlewentiguanjiancidebiaoge
 error_index = 0
-for txt_question_one in txt_question_2021:
-    form_str = ""
-    for key_file_one in key_file:
-        txt_question_one = os.path.join("./alltext/alltxt", key_file_one)
-        txt_question_list = open(txt_question_one, "r").readlines()
-        text_list[txt_question_one] = []
-        form_list[txt_question_one] = []
-        for txt_question_index in range(len(txt_question_list)):
-            txt_question_str = txt_question_list[txt_question_index]
+form_str = ""
+for key_file_one in key_file:
+    txt_question_one = os.path.join("./alltext/alltxt", key_file_one)
+    txt_question_list = open(txt_question_one, "r").readlines()
+    text_list[txt_question_one] = []
+    form_list[txt_question_one] = []
+    for txt_question_index in range(len(txt_question_list)):
+        txt_question_str = txt_question_list[txt_question_index]
+        try:
+            txt_question_eval = json.loads(txt_question_str)
+        except:
+            txt_question_eval = {}
+        if 'inside' in txt_question_eval:
             try:
-                txt_question_eval = json.loads(txt_question_str)
+                inside_eval = txt_question_eval['inside']
+                # print(inside_eval)
+                # print(type(inside_eval))
             except:
-                txt_question_eval = {}
-            if 'inside' in txt_question_eval:
-                try:
-                    inside_eval = eval(txt_question_eval['inside'])
-                except:
-                    inside_eval = ""
-
-            else:
                 inside_eval = ""
-            if inside_eval:
-                if isinstance(inside_eval, list):
-                    # first_label.extend(txt_question_eval)
-                    inside_eval_str = []
-                    for inside_eval_inner in inside_eval:
-                        inside_eval_str.append(str(inside_eval_inner))
-                    try:
-                        if len(inside_eval_str) > 1:
-                            form_str += "|" + "|".join(inside_eval_str) + "|\n"
-                    except:
-                        print(inside_eval_str)
-                else:
-                    if isinstance(inside_eval, str):
-                        text_list[txt_question_one].append(inside_eval)
-                    error_index += 1
-                    if error_index >= 2:
-                        if len(form_str):
-                            # for keyword_one in keyword_list:
-                            #     if keyword_one in form_str:
-                            form_list[txt_question_one].append(form_str)
-                            form_str = ""
-                            error_index = 0
+
+        else:
+            inside_eval = ""
+        if "['" in inside_eval:
+            inside_eval = list(eval(inside_eval))
+        if inside_eval:
+            if isinstance(inside_eval, list):
+                # first_label.extend(txt_question_eval)
+                inside_eval_str = []
+                for inside_eval_inner in inside_eval:
+                    inside_eval_str.append(str(inside_eval_inner))
+                try:
+                    if len(inside_eval_str) > 1:
+                        form_str += "|" + "|".join(inside_eval_str) + "|\n"
+                except:
+                    print(inside_eval_str)
+            else:
+                if isinstance(inside_eval, str):
+                    text_list[txt_question_one].append(inside_eval)
+                error_index += 1
+                if error_index >= 2:
+                    if len(form_str):
+                        # for keyword_one in keyword_list:
+                        #     if keyword_one in form_str:
+                        form_list[txt_question_one].append(form_str)
+                        form_str = ""
+                        error_index = 0
 # first_label.append(form_list)
 # form_list = {}
 for form_key, form_value in form_list.items():
